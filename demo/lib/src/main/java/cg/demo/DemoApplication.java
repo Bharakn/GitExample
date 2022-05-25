773fb153de56a711a987144e9160806f2196d6b9
Bharakn <103725471+Bharakn@users.noreply.github.com>
2022-05-25 20:19:33 +0530

    bharathupdate

--- demo/lib/src/main/java/cg/demo/DemoApplication.java
+++ demo/lib/src/main/java/cg/demo/DemoApplication.java
@@ -9,6 +9,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("this is venkatesh");}
+		System.out.println("this is venkatesh");
+		System.out.println("This is bharath");
+		
+	
+	}
 
 }
